94fdcd86410f34e9fb158b216a49ef498d9a0b01
KrisD1p <43444253+KrisD1p@users.noreply.github.com>
2019-03-26 09:49:56 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".witch").click(function() {
+    $(".witch").slideToggle();
+});
+$(".Boom").click(function() {
+    $(".Boom").fadeOut();
+});
+$(".scoop").click(function() {
+    $(".scoop").toggle();
+});
